b54e66a07bb7c82d2904fb400b6000f33e59536b
Andrey Dodonov <aadodonov@gmail.com>
2018-07-13 14:48:55 +0200

    basic linting and adjusting playground

--- Linky/test_requests.py
+++ Linky/test_requests.py
@@ -1,11 +1,10 @@
-from pip._vendor import requests # absolutely awesome library with "HTTP Requests for Humans"
+from pip._vendor import requests  # absolutely awesome library with "HTTP Requests for Humans"
 
 proxies = {
-  'http': 'http://i00109058:Salerdo100500@proxy.endress.com:8080',
-  'https': 'https://i00109058:Salerdo100500@proxy.endress.com:8080',
+    'http': 'http://i00109058:Salerdo100500@proxy.endress.com:8080',
+    'https': 'https://i00109058:Salerdo100500@proxy.endress.com:8080',
 }
 
-if __name__=="__main__":
+if __name__ == "__main__":
     r = requests.get('http://unbolt.me', proxies=proxies)
     print(r.text)
-
